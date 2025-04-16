3e42c2c8179f619d1e75045a963febbafb9a4b28
nandygavas <nandygavas@gmail.com>
2025-04-16 16:16:38 +0000

    commit

--- src/components/league/CreateLeagueModal.tsx
+++ src/components/league/CreateLeagueModal.tsx
@@ -1,3 +1,4 @@
+tsx
 import { useEffect, useState, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
